96bce8eb4fee0ce5307c88af90e298af3dd5f823
KrisjanisP <krisjanispetrucena@gmail.com>
2024-08-08 12:57:03 +0300

    a

--- design/users.go
+++ design/users.go
@@ -155,47 +155,6 @@ var _ = dsl.Service("users", func() {
 		dsl.Error("UsernameExistsConflict", dsl.String, "Username already exists")
 		dsl.Error("EmailExistsConflict", dsl.String, "Email already exists")
 	})
-
-	dsl.Method("updateUser", func() {
-		dsl.Description("Update an existing user")
-		dsl.Security(JWTAuth, func() {
-			dsl.Scope("users:write")
-		})
-		dsl.Payload(func() {
-			dsl.Token("token", dsl.String, "JWT token used for authentication", func() {
-				dsl.Example("jwt_token")
-			})
-			dsl.Attribute("uuid", dsl.String, "UUID of the user", func() {
-				dsl.Example("550e8400-e29b-41d4-a716-446655440000")
-			})
-			dsl.Attribute("username", dsl.String, "Username of the user", func() {
-				dsl.Example("johndoe")
-			})
-			dsl.Attribute("email", dsl.String, "Email of the user", func() {
-				dsl.Format(dsl.FormatEmail)
-				dsl.Example("johndoe@example.com")
-			})
-			dsl.Attribute("firstname", dsl.String, "First name of the user", func() {
-				dsl.Example("John")
-			})
-			dsl.Attribute("lastname", dsl.String, "Last name of the user", func() {
-				dsl.Example("Doe")
-			})
-			dsl.Attribute("password", dsl.String, "Password of the user", func() {
-				dsl.Example("password123")
-			})
-			dsl.Required("token", "uuid", "username", "email", "firstname", "lastname")
-		})
-		dsl.Result(User)
-		dsl.HTTP(func() {
-			dsl.PUT("/users/{uuid}")
-			dsl.Param("token:Authorization")
-			dsl.Response(dsl.StatusOK)
-			dsl.Response(dsl.StatusBadRequest)
-		})
-		dsl.Error("InvalidUserDetails")
-	})
-
 	dsl.Method("deleteUser", func() {
 		dsl.Description("Delete a user")
 		dsl.Security(JWTAuth, func() {
