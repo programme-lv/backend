f38f724130bb71f05906382dcb23367e805dc37e
KrisjanisP <krisjanispetrucena@gmail.com>
2024-08-08 14:20:31 +0300

    progress with submission creation

---
+++ subm/ddbsubm.go
@@ -0,0 +1,44 @@
+package subm
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/guregu/dynamo/v2"
+)
+
+// SubmissionRow represents the user data structure.
+type SubmissionRow struct {
+	Uuid     string `dynamo:"uuid,hash"` // Primary key
+	UnixTime int64  `dynamo:"unix_timestamp"`
+	Content  string `dynamo:"content"`
+	Version  int64  `dynamo:"version"` // For optimistic locking
+}
+
+// DynamoDbSubmTable represents the DynamoDB table.
+type DynamoDbSubmTable struct {
+	ddbClient *dynamodb.Client
+	tableName string
+	submTable *dynamo.Table
+}
+
+// NewDynamoDbSubmTable initializes a new DynamoDbUsersTable.
+func NewDynamoDbSubmTable(ddbClient *dynamodb.Client, tableName string) *DynamoDbSubmTable {
+	ddb := &DynamoDbSubmTable{
+		ddbClient: ddbClient,
+		tableName: tableName,
+	}
+	db := dynamo.NewFromIface(ddb.ddbClient)
+	table := db.Table(ddb.tableName)
+	ddb.submTable = &table
+
+	return ddb
+}
+
+func (ddb *DynamoDbSubmTable) Save(ctx context.Context, subm *SubmissionRow) error {
+	// Increment the version number for optimistic locking
+	subm.Version++
+
+	put := ddb.submTable.Put(subm).If("attribute_not_exists(version) OR version = ?", subm.Version-1)
+	return put.Run(ctx)
+}
--- subm/subms.go
+++ subm/subms.go
@@ -2,23 +2,104 @@ package subm
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/google/uuid"
+	"github.com/guregu/dynamo/v2"
 	submgen "github.com/programme-lv/backend/gen/submissions"
 	"goa.design/clue/log"
 )
 
 // submissions service example implementation.
 // The example methods log the requests and return zero values.
-type submissionssrvc struct{}
+type submissionssrvc struct {
+	ddbSubmTable *DynamoDbSubmTable
+}
 
 // NewSubmissions returns the submissions service implementation.
 func NewSubmissions() submgen.Service {
-	return &submissionssrvc{}
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("eu-central-1"),
+		config.WithSharedConfigProfile("kp"),
+	)
+	if err != nil {
+		panic(fmt.Sprintf("unable to load SDK config, %v", err))
+	}
+	dynamodbClient := dynamodb.NewFromConfig(cfg)
+
+	return &submissionssrvc{
+		ddbSubmTable: NewDynamoDbSubmTable(dynamodbClient, "ProglvTasks"),
+	}
+}
+
+type Validatable interface {
+	IsValid() error
+}
+
+type SubmissionContent struct {
+	Value string
+}
+
+func (subm *SubmissionContent) IsValid() error {
+	const maxSubmissionLength = 128000 // 128 KB
+	if len(subm.Value) > maxSubmissionLength {
+		return submgen.InvalidSubmissionDetails(
+			"maksimālais iesūtījuma garums ir 128 KB",
+		)
+	}
+	return nil
+}
+
+func (subm *SubmissionContent) String() string {
+	return subm.Value
 }
 
 // CreateSubmission implements submissions.Service.
-func (s *submissionssrvc) CreateSubmission(context.Context, *submgen.CreateSubmissionPayload) (res *submgen.Submission, err error) {
-	panic("unimplemented")
+func (s *submissionssrvc) CreateSubmission(ctx context.Context, p *submgen.CreateSubmissionPayload) (res *submgen.Submission, err error) {
+	submContent := SubmissionContent{Value: p.Submission}
+
+	for _, v := range []Validatable{&submContent} {
+		err := v.IsValid()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	uuid := uuid.New()
+	createdAt := time.Now().Unix()
+	row := &SubmissionRow{
+		Uuid:     uuid.String(),
+		UnixTime: createdAt,
+		Content:  submContent.String(),
+		Version:  0,
+	}
+
+	err = s.ddbSubmTable.Save(ctx, row)
+	if err != nil {
+		// TODO: automatically retry with exponential backoff on version conflict
+		if dynamo.IsCondCheckFailed(err) {
+			log.Errorf(ctx, err, "version conflict saving user")
+			return nil, submgen.InternalError("version conflict saving user")
+		} else {
+			log.Errorf(ctx, err, "error saving user")
+			return nil, submgen.InternalError("error saving user")
+		}
+	}
+
+	res = &submgen.Submission{
+		UUID:       row.Uuid,
+		Submission: row.Content,
+		Username:   "",
+		CreatedAt:  "",
+		Evaluation: nil,
+		Language:   nil,
+		Task:       nil,
+	}
+
+	return res, nil
 }
 
 // List all submissions
