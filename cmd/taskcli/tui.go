1f23e6f4d49c4b27b930630647a22f18e8a8fa8e
krisjanisp <krisjanispetrucena@gmail.com>
2024-09-15 18:27:25 +0300

    transform model

--- cmd/taskcli/tui.go
+++ cmd/taskcli/tui.go
@@ -15,15 +15,16 @@ const (
 )
 
 type model struct {
-	state       state
-	dir         string
-	uploadModel uploadModel
+	state          state
+	uploadTaskDir  string
+	uploadModel    uploadModel
+	transformModel transformModel
 }
 
 func initialModel(dir string) model {
 	return model{
-		state: stateMenu,
-		dir:   dir,
+		state:         stateMenu,
+		uploadTaskDir: dir,
 	}
 }
 
@@ -41,21 +42,26 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				return m, tea.Quit
 			case "1":
 				m.state = stateUpload
-				m.uploadModel = newUploadModel(m.dir)
+				m.uploadModel = newUploadModel(m.uploadTaskDir)
 				return m, m.uploadModel.Init()
 			case "2":
 				m.state = stateDelete
 				return m, nil
 			case "3":
 				m.state = stateTransform
-				return m, nil
+				m.transformModel = newTransformModel(m.uploadTaskDir)
+				return m, m.transformModel.Init()
 			}
 		}
 	case stateUpload:
 		var cmd tea.Cmd
 		m.uploadModel, cmd = m.uploadModel.Update(msg)
 		return m, cmd
-	case stateDelete, stateTransform:
+	case stateTransform:
+		var cmd tea.Cmd
+		m.transformModel, cmd = m.transformModel.Update(msg)
+		return m, cmd
+	case stateDelete:
 		// Handle input in delete and transform states
 		switch msg := msg.(type) {
 		case tea.KeyMsg:
@@ -76,14 +82,16 @@ func (m model) View() string {
 	switch m.state {
 	case stateMenu:
 		s := "Select an action:\n\n"
-		s += "1. Upload/Replace a task\n"
+		s += "1. Upload / replace a task\n"
 		s += "2. Delete a task with given ID\n"
-		s += "3. Transform from one file structure to another\n\n"
+		s += "3. Transform external file structure\n\n"
 		s += "Press q to quit.\n"
 		return s
 	case stateUpload:
 		return m.uploadModel.View()
-	case stateDelete, stateTransform:
+	case stateTransform:
+		return m.transformModel.View()
+	case stateDelete:
 		s := "Functionality not implemented yet.\n"
 		s += "Press 'b' to return to the main menu or 'q' to quit.\n"
 		return s
