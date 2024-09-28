1e35b20e51f74c069484f1c04dbc6e3748dd2627
krisjanisp <krisjanispetrucena@gmail.com>
2024-09-28 11:59:41 +0300

    add another task to lio2024 testdata

---
+++ fstask/lio2024/lio2024_test.go
@@ -0,0 +1,15 @@
+package lio2024_test
+
+import "testing"
+
+func TestParsingLio2024TaskWithoutAChecker(t *testing.T) {
+	// TODO: parse task ""
+}
+
+func TestParsingLio2024TaskWithAChecker(t *testing.T) {
+	// TODO:
+}
+
+func TestParsingLio2024TaskWithAnInteractor(t *testing.T) {
+	// TODO:
+}
