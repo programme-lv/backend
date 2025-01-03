6e093785a2cc42f8717c84060d2bdebe540007a5
krisjanisp <krisjanispetrucena@gmail.com>
2025-01-03 18:15:59 +0200

    reevaluating existing submissions

--- submsrvc/reeval.go
+++ submsrvc/reeval.go
@@ -2,105 +2,91 @@ package submsrvc
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/programme-lv/backend/evalsrvc"
 )
 
 func (s *SubmissionSrvc) ReevalSubm(ctx context.Context, submUuid uuid.UUID) error {
-	panic("not implemented")
-	// subm, err := s.GetSubm(ctx, submUuid)
-	// if err != nil {
-	// 	return fmt.Errorf("failed to get submission: %w", err)
-	// }
+	subm, err := s.GetSubm(ctx, submUuid)
+	if err != nil {
+		return err
+	}
 
-	// l, err := planglist.GetProgrammingLanguageById(subm.Lang.ShortID)
-	// if err != nil {
-	// 	return fmt.Errorf("failed to get programming language: %w", err)
-	// }
+	t, err := s.taskSrvc.GetTask(ctx, subm.Task.ShortID)
+	if err != nil {
+		return fmt.Errorf("failed to get task: %w", err)
+	}
 
-	// t, err := s.taskSrvc.GetTask(ctx, subm.Task.ShortID)
-	// if err != nil {
-	// 	return fmt.Errorf("failed to get task: %w", err)
-	// }
+	// enqueue evaluation into sqs for testing
+	evalUuid, err := s.evalSrvc.Enqueue(evalsrvc.CodeWithLang{
+		SrcCode: subm.Content,
+		LangId:  subm.Lang.ShortID,
+	}, s.evalReqTests(&t), evalsrvc.TesterParams{
+		CpuMs:      t.CpuMillis(),
+		MemKiB:     t.MemoryKiB(),
+		Checker:    t.CheckerPtr(),
+		Interactor: t.InteractorPtr(),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to enqueue evaluation: %w", err)
+	}
 
-	// // enqueue evaluation into sqs for testing
-	// evalUuid, err := s.evalSrvc.Enqueue(evalsrvc.CodeWithLang{
-	// 	SrcCode: subm.Content,
-	// 	LangId:  subm.Lang.ShortID,
-	// }, s.evalReqTests(&t), evalsrvc.TesterParams{
-	// 	CpuMs:      t.CpuMillis(),
-	// 	MemKiB:     t.MemoryKiB(),
-	// 	Checker:    t.CheckerPtr(),
-	// 	Interactor: t.InteractorPtr(),
-	// })
-	// if err != nil {
-	// 	return fmt.Errorf("failed to enqueue evaluation: %w", err)
-	// }
+	subtasks := []Subtask{}
+	for _, subtask := range t.Subtasks {
+		subtasks = append(subtasks, Subtask{
+			Points:      subtask.Score,
+			Description: subtask.Descriptions["lv"],
+			StTests:     subtask.TestIDs,
+		})
+	}
 
-	// subtasks := []Subtask{}
-	// for _, subtask := range t.Subtasks {
-	// 	subtasks = append(subtasks, Subtask{
-	// 		Points:      subtask.Score,
-	// 		Description: subtask.Descriptions["lv"],
-	// 		StTests:     subtask.TestIDs,
-	// 	})
-	// }
+	testgroups := []TestGroup{}
+	for i, tg := range t.TestGroups {
+		testgroups = append(testgroups, TestGroup{
+			Points:   tg.Points,
+			Subtasks: t.FindTestGroupSubtasks(i + 1),
+			TgTests:  tg.TestIDs,
+		})
+	}
 
-	// testgroups := []TestGroup{}
-	// for i, tg := range t.TestGroups {
-	// 	testgroups = append(testgroups, TestGroup{
-	// 		Points:   tg.Points,
-	// 		Subtasks: t.FindTestGroupSubtasks(i + 1),
-	// 		TgTests:  tg.TestIDs,
-	// 	})
-	// }
+	tests := []Test{}
+	for range t.Tests {
+		tests = append(tests, Test{})
+	}
 
-	// tests := []Test{}
-	// for range t.Tests {
-	// 	tests = append(tests, Test{})
-	// }
+	scoreUnit := ScoreUnitTest
+	if len(t.Subtasks) > 0 {
+		scoreUnit = ScoreUnitSubtask
+	}
+	if len(t.TestGroups) > 0 {
+		scoreUnit = ScoreUnitTestGroup
+	}
 
-	// scoreUnit := ScoreUnitTest
-	// if len(t.Subtasks) > 0 {
-	// 	scoreUnit = ScoreUnitSubtask
-	// }
-	// if len(t.TestGroups) > 0 {
-	// 	scoreUnit = ScoreUnitTestGroup
-	// }
+	ch, err := s.evalSrvc.Listen(evalUuid)
+	if err != nil {
+		return fmt.Errorf("failed to listen for evaluation updates: %w", err)
+	}
 
-	// ch, err := s.evalSrvc.Listen(evalUuid)
-	// if err != nil {
-	// 	return fmt.Errorf("failed to listen for evaluation updates: %w", err)
-	// }
+	eval := Evaluation{
+		UUID:       evalUuid,
+		Stage:      evalsrvc.StageWaiting,
+		ScoreUnit:  scoreUnit,
+		Error:      nil,
+		Subtasks:   subtasks,
+		Groups:     testgroups,
+		Tests:      tests,
+		Checker:    t.CheckerPtr(),
+		Interactor: t.InteractorPtr(),
+		CpuLimMs:   t.CpuMillis(),
+		MemLimKiB:  t.MemoryKiB(),
+		CreatedAt:  time.Now(),
+		SubmUUID:   submUuid,
+	}
+	s.inMem[submUuid] = eval
+	go s.handleUpdates(eval, ch)
 
-	// entity := SubmissionEntity{
-	// 	UUID:        submUuid,
-	// 	Content:     subm.Content,
-	// 	AuthorUUID:  subm.Author.UUID,
-	// 	TaskShortID: t.ShortId,
-	// 	LangShortID: l.ID,
-	// 	CurrEvalID:  evalUuid,
-	// 	CreatedAt:   subm.CreatedAt,
-	// }
-
-	// s.inMem[submUuid] = entity
-
-	// eval := Evaluation{
-	// 	UUID:       evalUuid,
-	// 	Stage:      evalsrvc.StageWaiting,
-	// 	ScoreUnit:  scoreUnit,
-	// 	Error:      nil,
-	// 	Subtasks:   subtasks,
-	// 	Groups:     testgroups,
-	// 	Tests:      tests,
-	// 	Checker:    t.CheckerPtr(),
-	// 	Interactor: t.InteractorPtr(),
-	// 	CpuLimMs:   t.CpuMillis(),
-	// 	MemLimKiB:  t.MemoryKiB(),
-	// 	CreatedAt:  time.Now(),
-	// }
-
-	// go s.handleUpdates(eval, ch)
-
-	// return nil
+	return nil
 }
