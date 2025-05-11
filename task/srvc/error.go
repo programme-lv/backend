61270092a847d40efd5ffdfed684e59610dedc7d
krisjanisp <krisjanispetrucena@gmail.com>
2025-05-11 14:08:13 +0300

    checkpoint

--- task/srvc/error.go
+++ task/srvc/error.go
@@ -2,6 +2,7 @@ package srvc
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/programme-lv/backend/common/srvcerror"
 )
@@ -22,4 +23,49 @@ func NewErrorImageAlreadyExists(filename string) *srvcerror.Error {
 		ErrCodeImageAlreadyExists,
 		fmt.Sprintf("attēls ar nosaukumu '%s' jau eksistē", filename),
 	)
+}
+
+const ErrCodeImageFileExtFromMimeType = "image_file_ext_from_mime_type"
+
+func NewErrorImageFileExtFromMimeType(mime string) *srvcerror.Error {
+	return srvcerror.New(
+		ErrCodeImageFileExtFromMimeType,
+		fmt.Sprintf("neizdevās iegūt attēla faila paplašinājumu [.png, .jpg, .jpeg, ...] no MIME tipa '%s'", mime),
+	)
+}
+
+const ErrCodeGetImageWidthAndHeight = "image_width_and_height"
+
+func NewErrorGetImageWidthAndHeight() *srvcerror.Error {
+	return srvcerror.New(
+		ErrCodeGetImageWidthAndHeight,
+		"neizdevās iegūt attēla platumu un augstumu [px]",
+	)
+}
+
+const ErrCodeImageInadequateDimensions = "image_inadequate_dimensions"
+
+func NewErrorImageInadequateDimensions() *srvcerror.Error {
+	return srvcerror.New(
+		ErrCodeImageInadequateDimensions,
+		"attēls ir pārāk mazs vai pārāk liels",
+	)
+}
+
+const ErrCodeFailedToGetTaskFromDb = "failed_to_get_task_from_db"
+
+func NewErrorFailedToGetTaskFromDb(taskId string) *srvcerror.Error {
+	return srvcerror.New(
+		ErrCodeFailedToGetTaskFromDb,
+		fmt.Sprintf("neizdevās iegūt uzdevumu '%s' no datubāzes", taskId),
+	)
+}
+
+const ErrCodeInternalServerError = "internal_server_error"
+
+func ErrInternalServerError() *srvcerror.Error {
+	return srvcerror.New(
+		ErrCodeInternalServerError,
+		"iekšēja servera kļūda",
+	).SetHttpStatusCode(http.StatusInternalServerError)
 }
