5650b6b3e99399eb1068183955bf5490aff7560a
KrisjanisP <krisjanispetrucena@gmail.com>
2025-09-25 14:05:03 +0300

    httpfunc pkg

---
+++ common/httpfunc/httpfunc.go
@@ -0,0 +1,90 @@
+package httpfunc
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"reflect"
+
+	"github.com/programme-lv/backend/common/jsonresp"
+	"github.com/programme-lv/backend/common/srvcerror"
+)
+
+type HandlerFuncImpl[Q any, R any] func(ctx context.Context, request Q) (response R, err error)
+
+func Json[Q any, R any](handler HandlerFuncImpl[Q, R]) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		var req Q
+		t := reflect.TypeOf(req)
+		if t.Size() > 0 {
+			if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+				jsonresp.BadRequest(w, err.Error())
+				return
+			}
+		}
+		result, err := handler(ctx, req)
+		if err != nil {
+			writeError(w, err)
+			return
+		}
+		jsonresp.Success(w, result)
+	}
+}
+
+type HandlerNoReq[R any] func(ctx context.Context) (response R, err error)
+
+func JsonNoReq[R any](handler HandlerNoReq[R]) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		result, err := handler(ctx)
+		if err != nil {
+			writeError(w, err)
+			return
+		}
+		jsonresp.Success(w, result)
+	}
+}
+
+type HandlerNoResp[Q any] func(ctx context.Context, request Q) (err error)
+
+func JsonNoResp[Q any](handler HandlerNoResp[Q]) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		var req Q
+		t := reflect.TypeOf(req)
+		if t.Size() > 0 {
+			if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+				jsonresp.BadRequest(w, err.Error())
+				return
+			}
+		}
+		if err := handler(ctx, req); err != nil {
+			writeError(w, err)
+			return
+		}
+		jsonresp.Success(w, struct{}{})
+	}
+}
+
+type HandlerNoReqNoResp func(ctx context.Context) (err error)
+
+func JsonNoReqNoResp(handler HandlerNoReqNoResp) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		if err := handler(ctx); err != nil {
+			writeError(w, err)
+			return
+		}
+		jsonresp.Success(w, struct{}{})
+	}
+}
+
+func writeError(w http.ResponseWriter, err error) {
+	e, ok := err.(*srvcerror.Error)
+	if !ok {
+		jsonresp.InternalError(w)
+		return
+	}
+	jsonresp.Error(w, e.Error(), e.HttpStatusCode(), e.ErrorCode())
+}
