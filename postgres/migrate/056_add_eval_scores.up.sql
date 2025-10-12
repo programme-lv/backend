2be76b6f3c4b5f043f8f1ce9058a523c8e15b7c6
krisjanisp <krisjanispetrucena@gmail.com>
2025-10-12 21:56:46 +0300

    add score caching columns to evaluations table

---
+++ postgres/migrate/056_add_eval_scores.up.sql
@@ -0,0 +1,10 @@
+ALTER TABLE evaluations
+    ADD COLUMN received_score INTEGER,
+    ADD COLUMN possible_score INTEGER,
+    ADD COLUMN scorebar_green INTEGER,
+    ADD COLUMN scorebar_red INTEGER,
+    ADD COLUMN scorebar_gray INTEGER,
+    ADD COLUMN scorebar_yellow INTEGER,
+    ADD COLUMN scorebar_purple INTEGER;
+
+
