4744729aa9c43a0b02b84ee1c28e9459a6171300
krisjanisp <krisjanispetrucena@gmail.com>
2026-07-05 18:05:06 +0300

    fix error in dump.sh

--- postgres/dump.sh
+++ postgres/dump.sh
@@ -103,7 +103,7 @@ pg_dump --version
 
 echo "Dumping ${DB} from ${HOST}:${PORT} to ${OUTFILE}..."
 
-read -p -r "Continue (y/n)? " choice
+read -r -p "Continue (y/n)? " choice
 case "$choice" in
 	y|Y ) echo "Proceeding...";;
 	n|N ) echo "Aborted."; exit 1;;
